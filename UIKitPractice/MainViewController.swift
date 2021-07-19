5b7de876c3b8c70ae465522f4168b4001b5006f3
SurbineHuang <>
2021-07-19 23:29:01 +0800

    First commit

---
+++ UIKitPractice/MainViewController.swift
@@ -0,0 +1,17 @@
+//
+//  MainViewController.swift
+//  UIKitPractice
+//
+//  Created by SurbineHuang on 19/7/21.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+     
+        self.view.backgroundColor = .lightGray
+    }
+}
